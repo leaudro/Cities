0ba7e24f0c7b33dff3cbd8e7ac6b21f6acdeb504
Leandro Santos <leaudro@gmail.com>
2018-06-13 23:27:22 -0300

    Improve data management by storing an index map to reduce search cost

--- app/src/main/java/com/leaudro/cities/model/DataSource.java
+++ app/src/main/java/com/leaudro/cities/model/DataSource.java
@@ -1,6 +1,7 @@
 package com.leaudro.cities.model;
 
 import android.os.AsyncTask;
+import android.support.v4.util.Pair;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -12,6 +13,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import static com.leaudro.cities.App.appInstance;
@@ -29,6 +31,50 @@ public class DataSource {
         Collections.sort(list);
 
         return list;
+    }
+
+    public HashMap<String, Pair<Integer, Integer>> indexMap;
+
+    private void createIndexMap() {
+        City firstCity = cities.get(0);
+        final char[] key = {firstCity.nameLowerCase.charAt(0), firstCity.nameLowerCase.charAt(1)};
+
+        indexMap = new HashMap<>();
+
+        int indexOneChar = 0, indexTwoChars = 0;
+        final int size = cities.size();
+        for (int i = 0; i < size; i++) {
+            City city = cities.get(i);
+            final char firstChar = city.nameLowerCase.charAt(0);
+            final char secondChar;
+            if (city.nameLowerCase.length() > 1) //There are cities with just one char (JP)
+                secondChar = city.nameLowerCase.charAt(1);
+            else secondChar = 0;
+
+            if (key[0] != firstChar) {
+                createIndexes(key, indexOneChar, indexTwoChars, i);
+
+                key[0] = firstChar;
+                key[1] = secondChar;
+
+                indexOneChar = i;
+                indexTwoChars = i;
+            } else if (key[1] != secondChar) {
+                createIndexes(key, -1, indexTwoChars, i);
+
+                key[1] = secondChar;
+                indexTwoChars = i;
+            }
+        }
+
+        createIndexes(key, indexOneChar, indexTwoChars, size);
+    }
+
+    private void createIndexes(char[] keys, int startFirstChar, int startSecondChar, int end) {
+        if (startFirstChar >= 0)
+            indexMap.put(String.valueOf(keys[0]), new Pair<>(startFirstChar, end));
+        if (keys[1] > 0 && startSecondChar >= 0)
+            indexMap.put(String.valueOf(keys), new Pair<>(startSecondChar, end));
     }
 
     public void setup(final OnSetupComplete callback) {
@@ -58,7 +104,10 @@ public class DataSource {
             if (dataSources == null) return null;
             for (DataSource dataSource : dataSources) {
                 dataSource.cities = dataSource.getCities();
+                dataSource.createIndexMap();
             }
+
+
             return null;
         }
     }
